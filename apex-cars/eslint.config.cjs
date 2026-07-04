eb7325620a4c0708ed90068b72825faf01817969
kalajiservice <kalajiservice2000@gmail.com>
2026-07-04 16:33:18 +0000

    ci(eslint): use empty flat config to prefer .eslintrc.cjs parsing

--- apex-cars/eslint.config.cjs
+++ apex-cars/eslint.config.cjs
@@ -1,11 +1,1 @@
-module.exports = [
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      ecmaFeatures: { jsx: true },
-    },
-    rules: {},
-  },
-];
+module.exports = [];
